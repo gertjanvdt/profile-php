fcbcd04724caac95dbf14ef79538ba3cae228e69
gertjanvdt <64344880+gertjanvdt@users.noreply.github.com>
2021-03-07 14:32:25 +0100

    updated routes

---
+++ src/app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('pages.index');
+    }
+
+    public function about()
+    {
+        return view('pages.about');
+    }
+
+    public function info()
+    {
+        return view('pages.info');
+    }
+}
---
+++ src/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('pages.test');
+// });
+
+// Route::get('/hello', function () {
+//     //return view('test');
+//     return 'hello world';
+// });
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/about', [HomeController::class, 'about'])->name('about');
+
+Route::get('/info', [HomeController::class, 'info'])->name('info');
+
+// wat doet de name function precies?
